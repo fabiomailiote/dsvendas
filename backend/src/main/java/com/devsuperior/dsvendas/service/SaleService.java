8a014c4e26e00fdb8253813a3684292cfc303eab
Fabio Augustus <bywebbho.me@gmail.com>
2022-04-02 16:42:58 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	/*
+		O parâmetro Pageable fará a paginação automaticamente
+		A notação 'Transactional' vai garantir que toda a operação com o banco seja resolvida no momento de execução do service. O parâmetro 'readOnly = true' é para evitar lock no banco. Como é uma operação de leitura não precisa fazer lock de escrita
+	 */
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
